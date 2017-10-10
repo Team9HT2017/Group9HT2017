e54dedca638ec289fd8a72e3ed5e78a7e2aa0f5a
debicce <gusdebfa@student.gu.se>
2017-10-10 18:10:15 +0200

    file chooser

--- JavaFx/src/Haus/Controller.java
+++ JavaFx/src/Haus/Controller.java
@@ -1,24 +1,20 @@
 package Haus;
 
-import com.sun.tools.hat.internal.model.Root;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.Node;
-
+import javafx.scene.control.ListView;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import java.awt.event.ActionEvent;
+import java.io.File;
 import java.io.IOException;
-import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -26,12 +22,22 @@ public class Controller implements Initializable {
 
     @FXML
     public Button uploadButton;
+
     @FXML
     public TextField IP;
+
     @FXML
     public Button connect;
+
     @FXML
     private GridPane Board;
+
+    @FXML
+    public ListView JsonList;
+
+    @FXML
+    public Button Animate;
+
 
     @FXML
     private void HandleButtonAction() throws IOException {
@@ -46,7 +52,7 @@ public class Controller implements Initializable {
         } else {
             try {
                 FXMLLoader fxmlloader =  new FXMLLoader(getClass().getResource("AnimationPage.fxml"));
-                Parent Root50 = (Parent) fxmlloader.load();
+                Parent Root50 =  fxmlloader.load();
                 Stage stage =new Stage();
                 stage.setTitle("Animation phase");
                 stage.setScene(new Scene(Root50));
@@ -58,11 +64,33 @@ public class Controller implements Initializable {
         }
 
     }
-        public void HandleButton () {
-            System.out.println("launch the server");
+    @FXML
+    private void HandleUploadAction() throws IOException {
 
+        FileChooser json = new FileChooser();
+        json.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Json Files", "*.json"));
+        File  SelectedFile = json.showOpenDialog(null);
+        if (SelectedFile != null ) {
+            JsonList.getItems().add(SelectedFile.getCanonicalFile());
 
+        } else {
+            System.out.println("File is not valid");
         }
+
+    }
+
+    @FXML
+    public void HandleButton () {
+        System.out.println("launch the server");
+
+
+    }
+    @FXML
+    private void HandleAnimation() throws IOException {
+
+        System.out.println("animation in progress");
+
+    }
 
 
     @Override
@@ -70,3 +98,11 @@ public class Controller implements Initializable {
 
     }
 }
+
+
+
+
+
+
+
+
--- JavaFx/src/Haus/Main.java
+++ JavaFx/src/Haus/Main.java
@@ -1,15 +1,20 @@
 package Haus;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
+
 public class Main extends Application {
+
 
 
     public static void main(String[] args) {
@@ -20,9 +25,15 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent Root = FXMLLoader.load(getClass().getResource("FrontPage.fxml"));
         primaryStage.setTitle("Haus");
-        primaryStage.setScene(new Scene(Root, 970, 500));
+        Scene scene =new Scene(Root, 970, 500);
+        primaryStage.setScene(scene);
         primaryStage.show();
+
+
+
     }
+
+
 
 
 }
