3d161dfda873e56d1140902fc1f7836c0130bf41
LaizFigueroa <gushecla@student.gu.se>
2017-10-25 09:44:34 +0200

    adding a Pid to the start on server

--- Server/erlServer/erlServer/src/erlServer_server.erl
+++ Server/erlServer/erlServer/src/erlServer_server.erl
@@ -41,18 +41,19 @@ init() ->
 %%--------------------------------------------------------------------
 start_server() ->
  io:format("Server started.~n"),
- {ok, ServerSocket} = gen_tcp:listen(?PORT, [binary, {packet, 0}, % packet, raw
+ Pid = spawn_link(fun() ->
+   {ok, ServerSocket} = gen_tcp:listen(?PORT, [binary, {packet, 0}, % packet, raw
    {reuseaddr, true}, {active, true}]),
   io:format("~p", [ServerSocket]),
-    server_loop(ServerSocket),
-ok.
+    server_loop(ServerSocket) end),
+  {ok, Pid}.
 
 server_loop(ServerSocket) ->
  {ok, Socket} = gen_tcp:accept(ServerSocket),
-  Pid = spawn(fun() -> erlServer_client:client() end),
+  Pid1 = spawn(fun() -> erlServer_client:client() end),
   inet:setopts(Socket, [{packet, 0}, binary, %% To change between active and passive modes when listening to messages.
     {nodelay, true}, {active, true}]),
-  gen_tcp:controlling_process(Socket, Pid), %% Let this PID guy take over my socket,
+  gen_tcp:controlling_process(Socket, Pid1), %% Let this PID guy take over my socket,
   %% He will be able to read and receive messages - Change ownership
   server_loop(ServerSocket).
 
