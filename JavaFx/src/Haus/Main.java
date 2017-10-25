5397480fbef302af8633c9f3f65314cc7cc96ffa
gusperlee <gusperlee@student.gu.se>
2017-10-25 14:15:25 +0200

    Added .metadata and .recommenders to .gitignore as well as fix to Main.java

--- JavaFx/src/Haus/Main.java
+++ JavaFx/src/Haus/Main.java
@@ -1,9 +1,5 @@
 package Haus;
-<<<<<<< HEAD
 
-
-=======
->>>>>>> 3d161dfda873e56d1140902fc1f7836c0130bf41
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -12,12 +8,7 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-<<<<<<< HEAD
-=======
-
 import java.io.IOException;
-
->>>>>>> 3d161dfda873e56d1140902fc1f7836c0130bf41
 
 public class Main extends Application {
 
@@ -26,13 +17,9 @@ public class Main extends Application {
 
 
         launch(args);
-<<<<<<< HEAD
 
-
-=======
         Map t1 = Parser_v1.Parse2(Controller.toParse);
         System.out.println(t1);
->>>>>>> 3d161dfda873e56d1140902fc1f7836c0130bf41
     }
 
     @Override
@@ -42,10 +29,6 @@ public class Main extends Application {
         Scene scene = new Scene(Root, 700, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
-<<<<<<< HEAD
-=======
-
->>>>>>> 3d161dfda873e56d1140902fc1f7836c0130bf41
 
 
     }
