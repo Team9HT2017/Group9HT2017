44a0a3df4771f583b9c2e4bae258c6e05157ecf5
Anthony1911 <guspathan@student.gu.se>
2017-10-26 14:30:21 +0200

    Attempt to delete unneeded stuff

--- JavaFX/src/Haus/SplashController.java
+++ JavaFX/src/Haus/SplashController.java
@@ -5,15 +5,16 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.layout.StackPane;
 import javafx.scene.Parent;
-import java.util.logging.Level;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
 
 //@Author Fahd ;
