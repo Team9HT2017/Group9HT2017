2557579e1b65f5e638e7d87ea60f0274004fadb8
LaizFigueroa <gushecla@student.gu.se>
2017-10-28 12:00:10 +0200

    Added comments on the supervisors

--- ehaus/src/ehaus_clients.erl
+++ ehaus/src/ehaus_clients.erl
@@ -2,8 +2,8 @@
 %%% @author LFigueroa
 %%% @copyright (C) 2017, Haus Project
 %%% @doc
-%%% This is part of the Supervisor Tree being a supervisor for
-%%% the client supervisor.
+%%% This is the second level of the Supervisor Tree being a supervisor
+%%% for the client supervisor.
 %%% @end
 %%% Created : 26. Oct 2017 12:58
 %%%-------------------------------------------------------------------
@@ -14,22 +14,53 @@
 -export([start_link/0]).
 -export([init/1]).
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called whenever ehaus_sup is started.
+%%
+%% @end
+%%--------------------------------------------------------------------
 start_link() ->
   supervisor:start_link({local, ?MODULE}, ?MODULE, none).
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called to start ehaus_client_sup supervisor and
+%% ehaus_client_manager worker under this supervisor with the definitions
+%% explained below.
+%%
+%% @end
+%%--------------------------------------------------------------------
 init(none) ->
-  RestartStrategy = {rest_for_one, 1, 60},
-  ClientSup = {ehaus_client_sup,
-    {ehaus_client_sup, start_link, []},
-    permanent,
-    5000,
-    supervisor,
-    [ehaus_client_sup]},
-  ClientMan = {ehaus_client_manager,
-    {ehaus_client_manager, start_link, []},
-    permanent,
-    5000,
-    worker,
-    [ehaus_client_manager]},
+  RestartStrategy = {rest_for_one, 1, 60}, %% If you need to restart a
+  %% process that is dependent on each other in a chain. X works alone,
+  %% but Y depend on it and Z depend on both. It restart the dependent
+  %% processes not the other way around.
+
+  ClientSup = {ehaus_client_sup, %% ChildId - Used for debugging
+    {ehaus_client_sup, start_link, []}, %% Start function -> {Module,
+    %% Function, Argument}
+    permanent,%% Restart = permanent -> It is a process that always
+    %% should be restarted, no matter what. Used by vital, long-living
+    %% processes.
+    5000, %% Shutdown
+    supervisor,%% Type = supervisor
+    [ehaus_client_sup]}, %% Modules -> The name of the callback module
+    %% used by the child behaviour
+
+  ClientMan = {ehaus_client_manager, %% ChildId - Used for debugging
+    {ehaus_client_manager, start_link, []}, %% Start function -> {Module,
+    %% Function, Argument}
+    permanent, %% Restart = permanent -> It is a process that always
+    %% should be restarted, no matter what. Used by vital, long-living
+    %% processes.
+    5000, %% Shutdown
+    worker,%% Type = worker
+    [ehaus_client_manager]}, %% Modules -> The name of the callback
+    %% module used by the child behaviour
+
   Children  = [ClientSup, ClientMan],
+
   {ok, {RestartStrategy, Children}}.
--- ehaus/src/ehaus_sup.erl
+++ ehaus/src/ehaus_sup.erl
@@ -2,7 +2,7 @@
 %%% @author LFigueroa
 %%% @copyright (C) 2017, Haus Project
 %%% @doc
-%%% This is part of the Supervisor Tree being a supervisor for
+%%% This is top-level of the Supervisor Tree being a supervisor for
 %%% the client supervisor.
 %%% @end
 %%% Created : 26. Oct 2017 10:39
@@ -14,16 +14,42 @@
 -export([start_link/0]).
 -export([init/1]).
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called whenever an application is started on ehaus
+%% module.
+%%
+%% @end
+%%--------------------------------------------------------------------
 start_link() ->
   supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called to start ehaus_clients supervisor under
+%% this supervisor with the definitions explained below.
+%%
+%% @end
+%%--------------------------------------------------------------------
 init([]) ->
-  RestartStrategy = {one_for_one, 1, 60},
-  Clients   = {ehaus_clients,
-    {ehaus_clients, start_link, []},
-    permanent,
-    5000,
-    supervisor,
-    [ehaus_clients]},
+  RestartStrategy = {one_for_one, 1, 60}, %% If one fail just that one
+  %% restart. Use it to supervise an independent process or when you can
+  %% lose the actual state without impacting others. It holds a list of
+  %% all the children it has started, ordered by their starting order.
+
+  Clients   = {ehaus_clients, %% ChildId -> Used for debugging
+    {ehaus_clients, start_link, []}, %% Start function -> {Module,
+    %% Function, Argument}
+    permanent, %% Restart = permanent -> It is a process that always
+    %% should be restarted, no matter what. Used by vital, long-living
+    %% processes.
+    5000, %% Shutdown
+    supervisor, %% Type = supervisor
+    [ehaus_clients]}, %% Modules -> The name of the callback module used
+    %% by the child behaviour
+
   Children  = [Clients],
+
   {ok, {RestartStrategy, Children}}.
