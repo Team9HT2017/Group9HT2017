62b4422f6138b81486b7f6f6697d2e23a2864e15
LaizFigueroa <gushecla@student.gu.se>
2017-10-29 14:47:43 +0100

    Adding comments on ehaus.erl

--- ehaus/src/ehaus.erl
+++ ehaus/src/ehaus.erl
@@ -15,25 +15,84 @@
 -export([start/0, start/1]).
 -export([start/2, stop/1]).
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called whenever an application is started using
+%% application:start/[Port] as a auxiliary function or by the normal
+%%
+%% @end
+%%--------------------------------------------------------------------
 listen(PortNum) ->
     ehaus_client_manager:listen(PortNum).
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by the application in order to ignore a
+%% client, calling the action on the server ehaus_client_manager.
+%%
+%% @end
+%%--------------------------------------------------------------------
 ignore() ->
     ehaus_client_manager:ignore().
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called whenever an application is started using
+%% application:start/[Port] as a auxiliary function or by the normal
+%% application:start/0. It should start the processes of the
+%% application. According to the OTP design principles it starts the
+%% top supervisor of the tree and also make the manager listen to the
+%% port
+%%
+%% @end
+%%--------------------------------------------------------------------
 start() ->
     application:ensure_started(sasl),
     application:start(?MODULE),
-    io:format("Starting es...").
+    io:format("Starting server...").
 
-
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called whenever an application is started using
+%% application:start/[Port], and it should start the processes of the
+%% application. According to the OTP design principles it starts the
+%% top supervisor of the tree and also make the manager listen to the
+%% port
+%%
+%% @end
+%%--------------------------------------------------------------------
 start(Port) ->
     ok = start(),
     ok = ehaus_client_manager:listen(Port),
+    %% ok = function -> always check that the returned value was the expected
+    %% one, and not invisible errors happening in the background.
     io:format("Startup complete, listening on ~w~n", [Port]).
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called whenever an application is started using
+%% application:start/[normal,2], and should start the processes of the
+%% application. According to the OTP design principles it starts the
+%% top supervisor of the tree.
+%%
+%% @end
+%%--------------------------------------------------------------------
 start(normal, _Args) ->
     ehaus_sup:start_link().
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called whenever an application has stopped. It
+%% is intended to be the opposite of Module:start/2 and should do
+%% any necessary cleaning up. The return value is ignored.
+%%
+%% @end
+%%--------------------------------------------------------------------
 stop(_State) ->
     ok.
