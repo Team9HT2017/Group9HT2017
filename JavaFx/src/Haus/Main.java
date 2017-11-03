95760fb676b40fca81783db600cf190418ad3dd8
LaizFigueroa <gushecla@student.gu.se>
2017-11-03 16:57:20 +0100

    Starting to fix defects on main (Comments, Dialog and Window size).

--- JavaFx/src/Haus/Main.java
+++ JavaFx/src/Haus/Main.java
@@ -1,22 +1,33 @@
 package Haus;
 
-
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
-
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Map;
 import java.util.Optional;
 
+/**
+ * Class to start the application, sets its basic characteristics.
+ *
+ * @author
+ * @version 1.0
+ *
+ * @editor Laiz Figueroa
+ * @version 1.1
+ * Modifications: Added comments, Dialog when closing the application more personalised,
+ * different size management for the application.
+ *
+ */
 
 public class Main extends Application {
 
@@ -30,12 +41,21 @@ public class Main extends Application {
         System.out.println(t1);
 
     }
-
+    /**
+     * Method to start the front page and define its size characteristics.
+     *
+     * @param primaryStage
+     * @throws Exception
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
+
         Parent Root = FXMLLoader.load(getClass().getResource("FrontPage.fxml"));
-        primaryStage.setTitle("Sample");
-        Scene scene = new Scene(Root, 700, 600);
+        primaryStage.setTitle("Haus Diagram Simulator");
+        //To get the application user's screen size and pass it to the set the application size
+        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+        Scene scene = new Scene(Root, screenBounds.getHeight() + 200, screenBounds.getHeight());
+
         primaryStage.setScene(scene);
         primaryStage.show();
 
@@ -43,28 +63,47 @@ public class Main extends Application {
             e.consume();
             closeprogram(primaryStage);
         });
-
     }
+
+    /**
+     * Method to get the teacher's IP to display to the users in order to
+     * connect to the server.
+     *
+     * @param text
+     *
+     */
     public void getIP(TextField text) throws UnknownHostException {
-        text.setText(String.valueOf(InetAddress.getLocalHost()));
-
+        text.setText(String.valueOf(InetAddress.getLocalHost().getHostAddress()));
     }
 
+    /**
+     * Method to display a dialog when the user try to close the application,
+     * then it asks if they user is aware of what he is doing. This will close
+     * safely the application.
+     *
+     * @param stage
+     *
+     */
     private void closeprogram(Stage stage) {
 
         Alert alert = new Alert(AlertType.CONFIRMATION);
-        alert.setTitle("Confirmation Dialog");
-        alert.setHeaderText("Look, a Confirmation Dialog");
-        alert.setContentText("Are you ok with this?");
+        alert.setTitle("Leaving so soon!");
+        alert.setHeaderText(null);
+        alert.setContentText("Do you really want to leave the application?");
 
+        // To modify the kind of buttons we have on the dialog
+        ButtonType buttonTypeYes = new ButtonType("Yes");
+        ButtonType buttonTypeCancel = new ButtonType("No");
+        // To add the buttons defined above to the dialog
+        alert.getButtonTypes().setAll(buttonTypeYes, buttonTypeCancel);
+
+        // To perform the close application when yes is selected
         Optional<ButtonType> result = alert.showAndWait();
-        if (result.get() == ButtonType.OK){
+        if (result.get() == buttonTypeYes){
             stage.close();
-            // ... user chose OK
+            // ... user chose Yes
         } else {
-            // ... user chose CANCEL or closed the dialog
+            // ... user chose No or closed the dialog
         }
-
     }
-
 }
