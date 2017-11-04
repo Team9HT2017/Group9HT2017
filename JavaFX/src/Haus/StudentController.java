4e2cd0c922f78d79d8bc7056f0df7302645b9cef
LaizFigueroa <gushecla@student.gu.se>
2017-11-04 11:57:12 +0100

    Coping the functions into the buttons

--- JavaFX/src/Haus/StudentController.java
+++ JavaFX/src/Haus/StudentController.java
@@ -6,9 +6,14 @@ package Haus;
  */
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -18,6 +23,10 @@ public class StudentController implements Initializable {
     @FXML
     public Button animateButton;
 
+    @FXML
+    public static TextField classID;
+
+    private Stage stage = new Stage();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -26,28 +35,33 @@ public class StudentController implements Initializable {
     @FXML
     private void HandleAnimation() throws IOException {
 
-        try {
+        if (classID.getText().isEmpty()) {
 
-            System.out.println("animation in progress");
-
-            String teacherIp  = Controller.IP.getText();
-            TCPClient.main("student", teacherIp);
-
-            AnimationController.runAnim(Parser_v1.Parse2(Controller.toParse));
-            //showstage();
-            //yourProcess();
-
-
-        } catch (Exception e) {
             Alert alert = new Alert(Alert.AlertType.WARNING);
-            alert.setTitle("Class connection fails");
+            alert.setTitle("Validate IP ");
             alert.setHeaderText(null);
-            alert.setContentText("Connect to a class to go to animation");
+            alert.setContentText("type the server IP address");
             alert.showAndWait();
-            System.out.println(e);
+        } else {
+            try {
+                if (TeacherController.uploaded)
+                    showStage();
+                    classID.clear();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
         }
 
     }
+    private void showStage() throws IOException {
 
+        FXMLLoader fxmlloader =  new FXMLLoader(getClass().getResource("Splash.fxml"));
+        Parent root =  fxmlloader.load();
+        stage = new Stage();
+        stage.setTitle("Loading Animation ...");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 }
--- JavaFX/src/Haus/TeacherController.java
+++ JavaFX/src/Haus/TeacherController.java
@@ -1,18 +1,38 @@
 package Haus;
 
 /**
+ * This class will handle the teacher's interface, where he/she can
+ * upload a diagram into the system, get the Class identification
+ * and open a class for his/hers students.
  *
- * Created by LFigueroa on 04/11/17.
+ * @author
+ * @version 1.0
+ *
+ * @author Laiz Figueroa
+ * @version 1.1
+ * Modification: Created two new classes from the previous version.
+ *
  */
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Inet4Address;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class TeacherController implements Initializable {
 
@@ -23,29 +43,63 @@ public class TeacherController implements Initializable {
     public Button animateDiagramButton;
 
     @FXML
-    private void HandleAnimation() throws IOException {
+    public ListView<File> diagramPath;
+
+    public static String toParse;
+
+    private Stage stage = new Stage();
+
+    public static boolean uploaded = false;
+
+    @FXML
+    private void selectDiagram() throws Exception {
+
+        FileChooser json = new FileChooser();
+        json.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Json Files", "*.json"));
+        json.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Text files", "*.txt"));
+        File SelectedFile = json.showOpenDialog(null);
+        if (SelectedFile != null) {
+            diagramPath.getItems().add(SelectedFile.getCanonicalFile());
+            toParse = new Scanner(SelectedFile).useDelimiter("\\Z").next();
+            uploaded = true;
+
+        } else {
+            System.out.println("File is not valid");
+        }
+    }
+
+    @FXML
+    private void createAnimation() throws IOException {
 
         try {
 
-            System.out.println("animation in progress");
+            System.out.println("Animation in progress");
 
             String ip = Inet4Address.getLocalHost().getHostAddress();
             TCPClient.main("teacher", ip);
 
-            AnimationController.runAnim(Parser_v1.Parse2(Controller.toParse));
-            //showstage();
-            //yourProcess();
-
+            AnimationController.runAnim(Parser_v1.Parse2(toParse));
+            showStage();
 
         } catch (Exception e) {
             Alert alert = new Alert(Alert.AlertType.WARNING);
-            alert.setTitle("Load a file ");
+            alert.setTitle("Loading Error");
             alert.setHeaderText(null);
             alert.setContentText("Load the file to animate");
             alert.showAndWait();
             System.out.println(e);
 
         }
+    }
+
+    private void showStage() throws IOException {
+
+        FXMLLoader fxmlloader =  new FXMLLoader(getClass().getResource("Splash.fxml"));
+        Parent root =  fxmlloader.load();
+        stage = new Stage();
+        stage.setTitle("Loading Animation ...");
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 
     @Override
