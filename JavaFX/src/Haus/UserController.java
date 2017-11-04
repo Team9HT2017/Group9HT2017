64b3891b86e3c690d2a7276f7fedd140b5c31833
Rema-Salman <gussalmre@student.gu.se>
2017-11-04 16:27:14 +0100

    GUI connection and error handeling msgs

--- JavaFX/src/Haus/UserController.java
+++ JavaFX/src/Haus/UserController.java
@@ -1,58 +1,78 @@
 package Haus;
 
 /**
- * Class to give the user an specific interface for his/her needs.
+ * Class to give the user a specific interface for his/her needs.
  * The user can create a class or join one by clicking the buttons,
- * then he/seh will be redirected to a new interface.
+ * then he/she will be redirected to a new interface.
  *
  * @author Laiz Figueroa
  * @version 1.0
+ * 
+ * 
+ * @author Rema Salman
+ * @version 1.1
+ * Modification: Created the connection in the button actions with the error handling.
+ *
  *
  */
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class UserController implements Initializable {
+public class UserController {
+	Main main = new Main();
+	@FXML
+	public Button teacherButton;
 
-    @FXML
-    public Button teacherButton;
+	@FXML
+	public Button studentButton;
 
-    @FXML
-    public Button studentButton;
+	@FXML
+	AnchorPane first;
 
-    private Stage stage = new Stage();
+	/**
+	 * Description: button action, which change the UserSelection anchorPane into
+	 * the teacher's anchorePane.
+	 * 
+	 */
+	@FXML
+	private void ChangeToTeacherMain() {
+		try {
+			// adding TeacherMain anchorPane instead of the UserSelection anchorPane
+			first.getChildren().add(FXMLLoader.load(getClass().getResource("TeacherMain.fxml")));
+		} catch (IOException e) {
+			Alert alert = new Alert(Alert.AlertType.WARNING);
+			alert.setTitle("Loading Error");
+			alert.setHeaderText(null);
+			alert.setContentText("Something went wrong! please try again ..");
+			alert.showAndWait();
+			e.printStackTrace();
+		}
+	}
 
-    private void showStage(String fxml) throws IOException {
+	@FXML
+	private void ChangeToStudentMain() {
+		try {
+			first.getChildren().add(FXMLLoader.load(getClass().getResource("StudentMain.fxml")));
+		} catch (IOException e) {
+			Alert alert = new Alert(Alert.AlertType.WARNING);
+			alert.setTitle("Loading Error");
+			alert.setHeaderText(null);
+			alert.setContentText("Something went wrong! please try again ..");
+			alert.showAndWait();
+			e.printStackTrace();
+		}
 
-        FXMLLoader fxmlloader =  new FXMLLoader(getClass().getResource(fxml));
-        Parent root =  fxmlloader.load();
-        stage = new Stage();
-        stage.setScene(new Scene(root));
-        stage.show();
-
-    }
-    @FXML
-    private void ChangeToTeacherMain() throws IOException {
-        showStage("TeacherMain.fxml");
-
-    }
-    @FXML
-    private void ChangeToStudentMain() throws IOException {
-        showStage("StudentMain.fxml");
-
-    }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
-    }
+	}
 }
