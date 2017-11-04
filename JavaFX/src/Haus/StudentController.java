0d677ba8a2e08615ab3f01a5fd2adc24704809b6
LaizFigueroa <gushecla@student.gu.se>
2017-11-04 10:25:01 +0100

    Adding the buttons handlers, and the connection with fxml and java

--- JavaFX/src/Haus/StudentController.java
+++ JavaFX/src/Haus/StudentController.java
@@ -5,12 +5,11 @@ package Haus;
  * Created by LFigueroa on 04/11/17.
  */
 
-import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
-
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -24,4 +23,31 @@ public class StudentController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
 
     }
+    @FXML
+    private void HandleAnimation() throws IOException {
+
+        try {
+
+            System.out.println("animation in progress");
+
+            String teacherIp  = Controller.IP.getText();
+            TCPClient.main("student", teacherIp);
+
+            AnimationController.runAnim(Parser_v1.Parse2(Controller.toParse));
+            //showstage();
+            //yourProcess();
+
+
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Class connection fails");
+            alert.setHeaderText(null);
+            alert.setContentText("Connect to a class to go to animation");
+            alert.showAndWait();
+            System.out.println(e);
+
+        }
+
+    }
+
 }
--- JavaFX/src/Haus/TeacherController.java
+++ JavaFX/src/Haus/TeacherController.java
@@ -7,9 +7,10 @@ package Haus;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
-
+import java.io.IOException;
+import java.net.Inet4Address;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -20,6 +21,33 @@ public class TeacherController implements Initializable {
 
     @FXML
     public Button animateDiagramButton;
+
+    @FXML
+    private void HandleAnimation() throws IOException {
+
+        try {
+
+            System.out.println("animation in progress");
+
+            String ip = Inet4Address.getLocalHost().getHostAddress();
+            TCPClient.main("teacher", ip);
+
+            AnimationController.runAnim(Parser_v1.Parse2(Controller.toParse));
+            //showstage();
+            //yourProcess();
+
+
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Load a file ");
+            alert.setHeaderText(null);
+            alert.setContentText("Load the file to animate");
+            alert.showAndWait();
+            System.out.println(e);
+
+        }
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
