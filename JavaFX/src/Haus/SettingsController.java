88fde852c474b901aaff38ed4c85ddda53f26ad4
LaizFigueroa <gushecla@student.gu.se>
2017-11-06 10:25:24 +0100

    Working on Settings and adding a controller class to it

---
+++ JavaFX/src/Haus/SettingsController.java
@@ -0,0 +1,23 @@
+package Haus;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+
+/**
+ * Class to add functionality to the Settings page.
+ *
+ * @author Laiz Figueroa
+ * @version 1.0
+ *
+ */
+public class SettingsController {
+
+    @FXML
+    public Slider houseNameSlide;
+
+    @FXML
+    public Slider colorblindSlide;
+
+
+
+}
