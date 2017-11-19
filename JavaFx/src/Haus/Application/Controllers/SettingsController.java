084ddf736602d4dcfc91cef721b5d0ca213585ff
LaizFigueroa <gushecla@student.gu.se>
2017-11-19 20:36:21 +0100

    Fixed the new window size bug on the settigns

--- JavaFx/src/Haus/Application/Controllers/SettingsController.java
+++ JavaFx/src/Haus/Application/Controllers/SettingsController.java
@@ -1,9 +1,9 @@
 package Haus.Application.Controllers;
 
+import Haus.Application.Main;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.stage.Stage;
@@ -25,41 +25,23 @@ public class SettingsController {
 
     public static int names = 0;
 
-    /**
-     * Method to add functionality to the Apply button, which will repaint the
-     * canvas depending on the selection done by the user in the sliders.
-     *
-     * @throws Exception
-     *
-     */
     @FXML
     private void applySettingsSelection() throws Exception {
         Stage stage = (Stage) applyButton.getScene().getWindow();
 
         if (houseNameSlider.getValue() == 1) {
             names = 1;
-        }
-
-        else {
+        } else {
+            names =0;
             //To close the new window
             stage.close();
         }
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("../FXML/AnimationPage.fxml"));
         Parent root = (Parent) loader.load();
-        stage.setTitle("Settings");
-        stage.setScene(new Scene(root));
-        stage.setX(0);
-        stage.setY(10);
-        stage.show();
+        Main.getScene(root, stage);
 
         TeacherController.uploaded = false;
-        AnimationController controller = loader.getController();
-
-        controller.cleanAnimationCanvas();
-        controller.animate();
-
-
         names = 0;
 
     }
