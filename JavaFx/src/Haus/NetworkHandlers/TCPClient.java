11eb9e3eaed764457a78b738a5e833852cc11c9a
Anthony1911 <guspathan@student.gu.se>
2017-11-20 15:23:02 +0100

    Added user names assigning and storage
    Storage stores the {Port,"username+"} pair (plus is indicator to add "\n" to send username to java). I found doubles are better than triples because of possibility of client crash/restart, which leads to PID loss

--- JavaFx/src/Haus/NetworkHandlers/TCPClient.java
+++ JavaFx/src/Haus/NetworkHandlers/TCPClient.java
@@ -5,6 +5,9 @@ import Haus.Application.Parser;
 
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 /**
  * This class handles the connection between the application and the server.
  *
@@ -15,7 +18,9 @@ import java.net.*;
 
 public class TCPClient {
 public static  String fromServ="nope";
-    public static String main(String user, String ip) throws Exception {
+public static String teacherUsername="";
+    @SuppressWarnings("rawtypes")
+	public static String main(String user, String ip) throws Exception {
 
         String sentence; // string to hold messages
         String modifiedSentence; // string to receive messages
@@ -39,15 +44,27 @@ public static  String fromServ="nope";
             DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
             // here we receive msg from server
             BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-           // message = message.replaceAll("\"", "^");
-        //    sentence = inFromUser.readLine(); // here we get the parsed doc
-            outToServer.writeUTF(message + '\n'); // send it to server
+            Object [] users = Parser.Parse2(TeacherController.toParse,false).keySet().toArray();
+            String gateway = "";
+            List <Object> ordered = new ArrayList <Object>();
+            for (int i=0;i<users.length;i++){
+            	if (!users[i].toString().contains("Gateway")){
+            	  ordered.add(users[i]+"+"); //+ servers as indicator for Erlang to add "\n" in its place so that Java can read username later
+            	}else{
+            	gateway=users[i].toString();}
+            }
+           ordered.add(gateway+"+");
+            
+            
+          
+          
+          
+            outToServer.writeUTF(message+'\n'+"!^!"+ordered.toString().replace('[', ' ').replace(']', ' ')+"!?!"+"TEACHER\n"); // send it to server
             request = inRequest.readLine(); // Ask to send the file
             System.out.println("Request file" + request);
             fromServ = inFromServer.readLine(); // Receive the parsed file
-            System.out.println("FROM SERVER: " + fromServ);
-
+            System.out.println("Teacher username: " + fromServ.substring((fromServ.indexOf("g")-1),fromServ.length())); 
+            teacherUsername=fromServ;
 
 
             clientSocket.close();
--- ehaus/src/ehaus_client.erl
+++ ehaus/src/ehaus_client.erl
@@ -43,17 +43,33 @@ listen(Parent, Debug, ListenSocket) ->
 loop(Parent, Debug, State = #s{socket = Socket}) ->
   ok = inet:setopts(Socket, [{active, once}]),
   receive
-    {tcp, Socket, <<"GET\n">>} ->
-      ok = io:format("~p Client saying goodbye. Bye!~n", [self()]),
+    {tcp,Socket,<<"GETUSERNAME\n">>} -> % request from client to get user name
+      Username = userNameHandler:assignUserName(Socket),
+      ok = gen_tcp:send(Socket,Username),
+      loop(Parent, Debug, State);
+    {tcp, Socket, <<"GET\n">>} -> % request to get map
       M=mapStorage:get_map(),
+      io:format("~p Map: ~n", [M]),
       ok = gen_tcp:send(Socket,M ),
       ok = gen_tcp:shutdown(Socket, read_write),
       exit(normal);
-    {tcp, Socket, Message} ->
+    {tcp, Socket, InitialMessage} -> % all message requests
+      Identifier = string:split(InitialMessage,"!?!",all), % find out what client wants
+      [Message,Ident]=Identifier,
+      case Ident of <<"TEACHER\n">> -> % teacher wants to put user names and map
+        Content=string:split(Message,"!^!",all),
+        [Map,Usernames]=Content,
+        mapStorage:send(Map), % send map to storage
+        userNameHandler:putUserNames(Usernames), % send user name list to storage
+        Username = userNameHandler:assignUserName(Socket),
+        ok = gen_tcp:send(Socket, ["Your username: ", Username]),
+        loop(Parent, Debug, State);
+        <<"STUDENT\n">>-> % student wants to send message
       ok = io:format("~p received: ~tp~n", [self(), Message]),
       ok = gen_tcp:send(Socket, ["You sent: ", Message]),
-      mapStorage:send(Message),
-      loop(Parent, Debug, State);
+      loop(Parent, Debug, State)
+      % student messaging goes here
+        end;
     {tcp_closed, Socket} ->
       ok = io:format("~p Socket closed, retiring.~n", [self()]),
       exit(normal);
@@ -74,3 +90,42 @@ system_get_state(Misc) -> {ok, Misc}.
 
 system_replace_state(StateFun, Misc) ->
   {ok, StateFun(Misc), Misc}.
+
+
+
+%% Backup loop, please don't delete it for God's sake
+%%
+%% loop(Parent, Debug, State = #s{socket = Socket}) ->
+%%  ok = inet:setopts(Socket, [{active, once}]),
+%%  receive
+%%    {tcp,Socket,<<"GETUSERNAME\n">>} -> % request from client to get user name
+%%      Username = userNameHandler:assignUserName(Socket),
+%%      ok = gen_tcp:send(Socket,Username),
+%%      loop(Parent, Debug, State);
+%%    {tcp, Socket, <<"GET\n">>} -> % request to get map
+%%      M=mapStorage:get_map(),
+%%      ok = gen_tcp:send(Socket,M ),
+%%      ok = gen_tcp:shutdown(Socket, read_write),
+%%      exit(normal);
+%%    {tcp, Socket, InitialMessage} -> % all message requests
+%%      Identifier = string:split(InitialMessage,"!?!",all), % find out what client wants
+%%      [Message,Ident]=Identifier,
+%%      case Ident of <<"USR\n">> -> % teacher wants to put user names
+%%        userNameHandler:putUserNames(Message),
+%%        ok = gen_tcp:send(Socket,["Success\n"] ),
+%%        loop(Parent, Debug, State);
+%%        <<"MAP\n">>-> % teacher wants to put map
+%%          ok = io:format("~p received: ~tp~n", [self(), Message]),
+%%          ok = gen_tcp:send(Socket, ["You sent: ", Message]),
+%%          mapStorage:send(Message),
+%%          loop(Parent, Debug, State)
+%%      end;
+%%    {tcp_closed, Socket} ->
+%%      ok = io:format("~p Socket closed, retiring.~n", [self()]),
+%%      exit(normal);
+%%    {system, From, Request} ->
+%%      sys:handle_system_msg(Request, From, Parent, ?MODULE, Debug, State);
+%%    Unexpected ->
+%%      ok = io:format("~p Unexpected message: ~tp", [self(), Unexpected]),
+%%      loop(Parent, Debug, State)
+%%  end.
