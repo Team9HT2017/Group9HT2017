c443092121be02a217e13b1a035a2b12fbd21165
LaizFigueroa <gushecla@student.gu.se>
2017-12-02 20:48:35 +0100

    Cleaning code, reformating, fixing small bugs on User, Main, Teacher and Student controller

--- JavaFx/src/Haus/PresentationUI/Main.java
+++ JavaFx/src/Haus/PresentationUI/Main.java
@@ -5,11 +5,11 @@ import Haus.TechnicalFramework.Controllers.TeacherController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ButtonType;
-import javafx.stage.Screen;
-import javafx.stage.Stage;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import java.util.Optional;
@@ -35,7 +35,7 @@ import java.util.concurrent.Future;
  */
 
 public class Main extends Application {
-    
+
     public static Scene scene;
 
     public static void main(String[] args) {
