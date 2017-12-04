43232401711b25e47259e7f3f49fc82f8b83b204
Rema-Salman <gussalmre@student.gu.se>
2017-12-04 14:54:21 +0100

    dijkstra

--- JavaFx/src/Haus/TechnicalFramework/Controllers/AnimationController.java
+++ JavaFx/src/Haus/TechnicalFramework/Controllers/AnimationController.java
@@ -208,6 +208,7 @@ public class AnimationController implements Initializable {
         Random rand = new Random();
 
         System.out.println("Creating DrawableObjects");
+        nodes.clear();
         for (Object obj : map.keySet()) {
             if (TeacherController.user.equals("teacher")) {
                 nodes.add(new DrawableObject(obj, mapSize, mapSize));
